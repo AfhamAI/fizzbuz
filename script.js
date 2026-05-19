00f823c981ec945f6ffc8e69c68bb9f802a105c6
Afham <afhamtab@gmail.com>
2026-05-19 20:29:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function fizz(x){
+ if(x%5 ==0 && x%3 ==0){
+     console.log("FizzBuzz") 
+}
+   else if(x%3 == 0){
+ console.log("Fizz")
+}
+else if(x%5 ==0){
+    console.log("Buzz")
+}
+
+}
+
+fizz(9)
